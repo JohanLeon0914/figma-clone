47196edd26fe6e93509d1650a6d658eddedb1f38
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-02-03 11:08:18 -0500

    adding text, color and export to pdf features

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,9 +1,10 @@
-import { CustomFabricObject, RightSidebarProps } from "@/types/type"
-import Color from "./settings/Color"
-import Dimensions from "./settings/Dimensions"
-import Export from "./settings/Export"
-import Text from "./settings/Text"
-import { modifyShape } from "@/lib/shapes"
+import { CustomFabricObject, RightSidebarProps } from "@/types/type";
+import Color from "./settings/Color";
+import Dimensions from "./settings/Dimensions";
+import Export from "./settings/Export";
+import Text from "./settings/Text";
+import { modifyShape } from "@/lib/shapes";
+import { useRef } from "react";
 
 const RightSidebar = ({
   elementAttributes,
@@ -13,44 +14,62 @@ const RightSidebar = ({
   isEditingRef,
   syncShapeInStorage,
 }: RightSidebarProps) => {
+  const colorInputRef = useRef(null);
+  const strokeInputRef = useRef(null);
 
   const handleInputChange = (property: string, value: string) => {
-    if(!isEditingRef.current) isEditingRef.current = true;
+    if (!isEditingRef.current) isEditingRef.current = true;
 
     setElementAttributes((prev) => ({
-      ...prev, [property]: value
-    }))
+      ...prev,
+      [property]: value,
+    }));
 
     modifyShape({
       canvas: fabricRef.current as fabric.Canvas,
       property,
       value,
       activeObjectRef,
-      syncShapeInStorage
-    })
-
-  }
+      syncShapeInStorage,
+    });
+  };
 
   return (
-    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky right-0 h-full max-sm:hidden select-none">
-      <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
-      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+    <section className='min-2-[227px] sticky right-0 flex h-full select-none flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 max-sm:hidden'>
+      <h3 className='px-5 pt-4 text-xs uppercase'>Design</h3>
+      <span className='mt-3 border-b border-primary-grey-200 px-5 pb-4 text-xs text-primary-grey-300'>
         Make changes to canvas as your live
       </span>
 
-      <Dimensions 
+      <Dimensions
         isEditingRef={isEditingRef}
         width={elementAttributes.width}
         height={elementAttributes.height}
         handleInputChange={handleInputChange}
       />
-      <Text />
-      <Color />
-      <Color />
+      <Text
+        fontFamily={elementAttributes.fontFamily}
+        fontSize={elementAttributes.fontSize}
+        fontWeight={elementAttributes.fontWeight}
+        handleInputChange={handleInputChange}
+      />
+      <Color
+        inputRef={colorInputRef}
+        attribute={elementAttributes.fill}
+        attributeType='fill'
+        placeholder='Color'
+        handleInputChange={handleInputChange}
+      />
+      <Color
+        inputRef={strokeInputRef}
+        attribute={elementAttributes.stroke}
+        attributeType='stroke'
+        placeholder='Stroke'
+        handleInputChange={handleInputChange}
+      />
       <Export />
-
     </section>
-  )
-}
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
